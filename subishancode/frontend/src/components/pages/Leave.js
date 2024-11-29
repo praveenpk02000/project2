5a474ec95b5d03d0d357cdcd552f4ba86ce2a66c
praveenpk02000 <praveenpk02000@gmail.com>
2024-11-29 12:59:29 +0530

    first commit

---
+++ subishancode/frontend/src/components/pages/Leave.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Leave = () => {
+  return (
+    <div>
+      <h2>Leave</h2>
+    </div>
+  );
+};
+
+export default Leave;
---
+++ subishancode/frontend/src/components/pages/Performance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Performance = () => {
+  return (
+    <div>
+      <h2>Performance</h2>
+    </div>
+  );
+};
+
+export default Performance;
